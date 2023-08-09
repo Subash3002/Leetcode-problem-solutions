7147db0432d4a1a656b3b7eb23f2e71c8dbb6f2f
Subash AL <129574007+Subash3002@users.noreply.github.com>
2023-08-09 22:13:18 +0530

    Add files via upload

---
+++ Leetcode 121. Best Time to Buy and Sell Stock/lc 121.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=prices[0];
+        int profit=0;
+        int max=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }else{
+                profit=prices[i]-buy;
+                if(profit>max){
+                    max=profit;
+                }
+            }
+
+        }
+
+        return max;
+    }
+}
