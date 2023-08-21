7d88f706955344311efc7e139ef803180aa3d857
Subash AL <129574007+Subash3002@users.noreply.github.com>
2023-08-21 23:08:49 +0530

    Add files via upload

---
+++ Leetcode 232. Implement Queue using Stacks/lc 232.java
@@ -0,0 +1,54 @@
+class MyQueue {
+    Stack<Integer> first;
+    Stack<Integer> second;
+    public MyQueue() {
+        first=new Stack<>();
+        second=new Stack<>();
+    }
+    
+    public void push(int x) {
+        first.push(x);
+    }
+    
+    public int pop() {
+        while(!first.empty()){
+            second.push(first.pop());
+        }
+
+        int ele=second.pop();
+
+        while(!second.empty()){
+            first.push(second.pop());
+        }
+
+        return ele;
+        
+    }
+    
+    public int peek() {
+        while(!first.empty()){
+            second.push(first.pop());
+        }
+
+        int ele=second.peek();
+
+        while(!second.empty()){
+            first.push(second.pop());
+        }
+
+        return ele;
+    }
+    
+    public boolean empty() {
+        return first.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
