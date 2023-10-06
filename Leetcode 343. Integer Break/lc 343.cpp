d71cfc116022bfc46de884899f08416a44b38246
Subash AL <129574007+Subash3002@users.noreply.github.com>
2023-10-06 22:21:55 +0530

    Add files via upload

---
+++ Leetcode 343. Integer Break/lc 343.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n<4) return n-1;
+        int ans=1;
+        while(n>4){
+            ans*=3;
+            n-=3;
+        }
+        if(n!=0)ans*=n;
+        return ans;
+    }
+};
