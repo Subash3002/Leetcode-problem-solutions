d8068d26805fd9a6142cde8502b4ebb95b155ad0
Subash AL <129574007+Subash3002@users.noreply.github.com>
2023-11-17 23:21:50 +0530

    Add files via upload

---
+++ Leetcode 1877. Minimize Maximum Pair Sum in Array/lc 1877.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxx=0;
+        int n=nums.size();
+        for(int i=0;i<n/2;i++){
+            maxx=max(maxx,nums[i]+nums[n-1-i]);
+        }
+        return maxx;
+    }
+};
